a990bccc579fb4cd4bfd90e4b03d633ba3d4f605
Crowdbotics <team@crowdbotics.com>
2023-10-12 22:34:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "comeliketiktok-43908",
+  url: "https://comeliketiktok-43908.botics.co",
+  api: "https://comeliketiktok-43908.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
